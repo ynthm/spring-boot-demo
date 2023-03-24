b106869bab473c50234e6ad2c8309d8593f0896a
wangyinsheng <wangys@veiban.com>
2023-03-24 21:02:09 +0800

    feat: add util

--- ynthm-common/common-core/src/main/java/com/ynthm/common/util/CastUtil.java
+++ ynthm-common/common-core/src/main/java/com/ynthm/common/util/CastUtil.java
@@ -1,5 +1,8 @@
 package com.ynthm.common.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Ethan Wang
  * @version 1.0
@@ -11,5 +14,19 @@ public class CastUtil {
   @SuppressWarnings("unchecked")
   public static <T> T cast(Object obj) {
     return (T) obj;
+  }
+
+  public static <T> List<T> castList(Object obj, Class<T> clazz)
+  {
+    List<T> result = new ArrayList<>();
+    if(obj instanceof List<?>)
+    {
+      for (Object o : (List<?>) obj)
+      {
+        result.add(clazz.cast(o));
+      }
+      return result;
+    }
+    return null;
   }
 }
--- ynthm-common/common-core/src/main/java/com/ynthm/common/util/IoUtil.java
+++ ynthm-common/common-core/src/main/java/com/ynthm/common/util/IoUtil.java
@@ -3,7 +3,6 @@ package com.ynthm.common.util;
 import com.google.common.io.ByteSource;
 import com.google.common.io.CharStreams;
 import com.ynthm.common.exception.UtilException;
-
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -13,6 +12,7 @@ import java.nio.charset.StandardCharsets;
  * @version 1.0
  */
 public class IoUtil {
+  public static final int DEFAULT_BUFFER_SIZE = 8192;
 
   private IoUtil() {}
 
@@ -40,7 +40,22 @@ public class IoUtil {
     }
   }
 
-  public static InputStream inputStream(String initialString) {
-    return new ByteArrayInputStream(initialString.getBytes(StandardCharsets.UTF_8));
+  public static String toString(InputStream inputStream) throws IOException {
+    return toString(inputStream, StandardCharsets.UTF_8);
+  }
+
+  public static String toString(InputStream inputStream, Charset charset) throws IOException {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+    int length;
+    while ((length = inputStream.read(buffer)) != -1) {
+      out.write(buffer, 0, length);
+    }
+
+    return out.toString(charset.name());
+  }
+
+  public static InputStream toInputStream(String content) {
+    return new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
   }
 }
